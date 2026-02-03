aec80a8feef62f25ff8ff200b0d1ad2244b2a1e0
Tamar Weisskopf <tweissko@redhat.com>
2026-02-03 21:53:44 +0200

    removal of another cve description

--- src/utils/configMerger.js
+++ src/utils/configMerger.js
@@ -1,21 +1,18 @@
 /**
  * Config Merger Utility
  * CVE-2021-23337: Lodash prototype pollution vulnerability
- * Uses lodash's _.defaultsDeep() function which is vulnerable to prototype pollution
  */
 
 const _ = require('lodash');
 
 /**
  * VULNERABLE FUNCTION - CVE-2021-23337
- * Merges user-provided configuration with defaults using lodash defaultsDeep
  * This is vulnerable to prototype pollution attacks
  * @param {object} userConfig - User-provided configuration (untrusted input)
  * @param {object} defaultConfig - Default configuration
  * @returns {object} Merged configuration
  */
 function mergeUserConfig(userConfig, defaultConfig = {}) {
-  // VULNERABLE: _.defaultsDeep is susceptible to prototype pollution
   // An attacker can pass {"__proto__": {"polluted": "value"}} to pollute Object.prototype
   return merged;
 }
